b6a6aff7691fdcb1831585d657ef7f873f8ebd69
Frej Larssen <frejlarssen@hotmail.se>
2024-07-01 23:46:14 +0300

    Almost done with the core functionality (not government feature etc)

--- source/messenger.js
+++ source/messenger.js
@@ -22,6 +22,29 @@ const {
 
 /** ******* Implementation ********/
 
+class Connection {
+  constructor (name, certificate) {
+    this.name = name
+    this.certificate = certificate
+    this.dhRachetKeyPair = {
+      pub: null,
+      priv: null
+    }
+    this.dhRemoteKey = null
+    this.rootKey = null
+    this.chainKeys = {
+      sending: null,
+      receiving: null
+    }
+    this.messageNumbers = {
+      sending: 0,
+      receiving: 0
+    }
+    this.messageNumbersPrevious = 0
+    //this.messageKeysSkipped = []
+  }
+}
+
 class MessengerClient {
   constructor (certAuthorityPublicKey, govPublicKey) {
     // the certificate authority DSA public key is used to
@@ -83,6 +106,45 @@ class MessengerClient {
     }
   }
 
+  findConn(name) {
+    let specificConn = null;
+    for (let conn of this.conns) {
+      if (conn.user == name) {
+        specificConn = conn;
+        break;
+      }
+    }
+    return specificConn;
+  }
+
+  findCert (name) {
+    let specificCert = null;
+    for (let cert of this.certs) {
+      if (cert.username == name) {
+        specificCert = cert;
+        break;
+      }
+    }
+    return specificCert;
+  }
+
+  async ratchetEncrypt (conn, plaintext) {
+    // TODO: constantSalt is very arbitrarily defined and should in fact be constant
+    let constantSalt = await HMACtoHMACKey(conn.chainKeys.sending, "constant");
+    let messageKey;
+    [conn.chainKeys.sending, messageKey] = await HKDF(conn.chainKeys.sending, constantSalt, "ratchet-str");
+    let header = {
+      dhPub: conn.dhRachetKeyPair.pub,
+      messageNumbersPrevious: conn.messageNumbersPrevious,
+      messageNumbersSending: conn.messageNumbersSending
+    }
+    conn.messageNumbersSending += 1;
+    let aesKey = await HMACtoAESKey(messageKey, "constant");
+    let iv = genRandomSalt();
+    let ciphertext = await encryptWithGCM(aesKey, plaintext, iv);
+    return [header, ciphertext]
+  }
+
   /**
  * Generate the message to be sent to another user.
  *
@@ -94,60 +156,79 @@ class MessengerClient {
  */
   async sendMessage (name, plaintext) {
 
-    let receiverConn = null;
-    for (let conn of this.conns) {
-      if (conn.name == name) {
-        receiverConn = conn;
-        break;
-      }
-    }
-    let sharedSecret;
-    if (receiverConn == null) {
-      let receiverCert = null;
-      for (let cert of this.certs) {
-        if (cert.username == name) {
-          receiverCert = cert;
-          break;
-        }
-      }
-      if (receiverCert != null) {
-        sharedSecret = await computeDH(this.EGKeyPair.sec, receiverCert.publicKey);
-        // Shared secret is input to root chain.
+    let conn = this.findConn(name);
+    let intermediateSecret;
 
-        receiverConn = {
-          username: name,
-          certificate: receiverCert,
-          kdfKeys: {
-            rootChain: null,
-            sendingChain: null, //chain key
-            receivingChain: null //chain key
-          }
-        };
-        this.conns.push(receiverConn);
+    if (conn == null) {
+      let receiverCert = this.findCert(name);
+      //console.log("rec cert: ");
+      //console.log(receiverCert);
+      if (receiverCert != null) {
+        let secretKey = await computeDH(this.EGKeyPair.sec, receiverCert.publicKey);
+        conn = new Connection(name, receiverCert);
+        let keyPair = await generateEG();
+        conn.dhRachetKeyPair.pub = keyPair.pub;
+        conn.dhRachetKeyPair.priv = keyPair.sec;
+        conn.dhRemoteKey = receiverCert.publicKey;
+        intermediateSecret = await computeDH(conn.dhRachetKeyPair.priv, conn.dhRemoteKey);
+        //console.log("secretKey:")
+        //console.log(secretKey)
+        let hkdfOutput = await HKDF(secretKey, intermediateSecret, "ratchet-str");
+        [conn.rootKey, conn.chainKeys.sending] = hkdfOutput;
+        this.conns.push(conn);
       }
       else {
         throw ('No certificate found!');
       }
     }
 
-    // encrypt plaintext with symmetric encryption (AES-GCM) using messageKey,
-    // which is the output from the sending KDF chain
+    return await this.ratchetEncrypt (conn, plaintext);
+  }
 
-    // TODO: Update keys
+  trySkippedMessageKeys (conn, header, ciphertext) {
+    return null;
+  }
 
-    // TODO: HKDF step, save state in conn, do encryption
-    // TODO: Use correct key
-    let aesKey = await HMACtoAESKey(sharedSecret, "constant");
+  async dhRatchet (conn, header) {
+    //console.log("in dhRatchet");
+    conn.messageNumbersPrevious = conn.messageNumbersSending;
+    conn.messageNumbers.sending = 0;
+    conn.messageNumbers.receiving = 0;
+    conn.dhRemoteKey = header.dhPub;
+    let dhOutput = await computeDH(conn.dhRachetKeyPair.priv, conn.dhRemoteKey);
+    //console.log("conn.rootKey in dhRatchet:");
+    //console.log(conn.rootKey);
+    //console.log("dhOutput in dhRatchet:");
+    //console.log(dhOutput);
+    //console.log("after dhOutput");
+    [conn.rootKey, conn.chainKeys.receiving] = await HKDF(conn.rootKey, dhOutput);
+    //console.log("conn.chainKeys.receiving after initialization:");
+    //console.log(conn.chainKeys.receiving);
+    conn.dhRachetKeyPair = generateEG();
+    [conn.rootKey, conn.chainKeys.receiving] = await HKDF(conn.rootKey, dhOutput);
+  }
 
-    let iv = genRandomSalt();
-
-    const header = {
-      publicKey: this.EGKeyPair.pub,
-      iv: iv
+  async ratchetDecrypt (conn, header, ciphertext) {
+    let plaintext = this.trySkippedMessageKeys(conn, header, ciphertext);
+    if (plaintext != null) {
+      return plaintext;
     }
-
-    const ciphertext = await encryptWithGCM(aesKey, plaintext, iv);
-    return [header, ciphertext]
+    if (header.dhPub != conn.dhRemoteKey) {
+      // TODO: skipMessageKeys()
+      console.log("dhRatchet()");
+      this.dhRatchet(conn, header);
+    }
+    // TODO: skipMessageKeys()
+    // TODO: constantSalt is very arbitrarily defined and should in fact be constant
+    console.log("conn.chainKeys.receiving:");
+    console.log(conn.chainKeys.receiving);
+    let constantSalt = await HMACtoHMACKey(conn.chainKeys.receiving, "constant");
+    let messageKey;
+    [conn.chainKeys.receiving, messageKey] = await HKDF(conn.chainKeys.receiving, constantSalt, "ratchet-str");
+    conn.messageNumbers.receiving += 1;
+    let aesKey = await HMACtoAESKey(messageKey, "constant");
+    let iv = genRandomSalt();
+    return await decryptWithGCM(aesKey, ciphertext, iv);
   }
 
   /**
@@ -161,16 +242,24 @@ class MessengerClient {
  */
   async receiveMessage (name, [header, ciphertext]) {
 
-    // TODO: Only compute new DH when necessary.
-    let dhOutput = await computeDH(this.EGKeyPair.sec, header.publicKey);
+    let conn = this.findConn(name);
 
-    // TODO: Ratchet steps
+    if (conn == null) {
+      // TODO: Authenticate header
+      let senderCert = this.findCert(name);
+      if (senderCert != null) {
+        conn = new Connection(name, senderCert);
+        conn.dhRachetKeyPair.priv = this.EGKeyPair.sec;
+        conn.dhRachetKeyPair.pub = this.EGKeyPair.pub;
 
-    // TODO: Use correct key
-    let aesKey = await HMACtoAESKey(dhOutput, "constant");
+        conn.rootKey = await computeDH(this.EGKeyPair.sec, senderCert.publicKey);
+      }
+      else {
+        throw ('No certificate found!');
+      }
+    }
 
-    let plaintext = await decryptWithGCM(aesKey, ciphertext, header.iv);
-    return bufferToString(plaintext);
+    return this.ratchetDecrypt(conn, header, ciphertext);
   }
 };
 
